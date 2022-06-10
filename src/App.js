6d26dfa7b880bb1a35e460b8aeaacacb5a833d54
unknown <srroy119@gmail.com>
2022-06-10 23:23:36 +0530

    style commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ function App() {
   return (
     <Container>
       <Header>
-      <BarC className='tt1' ><BarCharts /></BarC>
-     <BarC className='tt2'><LineGraph /></BarC>
+      <BarC className='tt1' ><BarCharts/></BarC>
+     <Line1 className='tt2'><LineGraph/></Line1>
+     <Line2 className='tt3'></Line2>
+     <Line3 className='tt4'></Line3>
+     <Line4 className='tt5'></Line4>
      </Header>
       <h1>hi</h1>
     </Container>
@@ -30,24 +33,72 @@ margin-top: 20px;
 `
 
 const BarC = styled.div`
-  //width: 800px;
-  //height: 500px;
+  width: 800px;
+  height: 500px;
   background-color: white;
   border-radius: 20px;
   padding:20px;
-  border:1px solid #ebe9f1
+  border:1px solid #ebe9f1;
+  margin: 20px 0 0 20px;
 `
 
 const Header = styled.div`
 display: grid;
+gap: 20px;
+//height: 140vh;
+background-color rgb(50, 168, 12, 1);
 //justify-content: space-between;
     .tt1{
-      grid-column: 1 / span 1;
-      grid-row: 1;
+      grid-column: 1 ;
+      grid-row: 1 / span 1;
     }
     .tt2{
-      grid-column: 3;
-      grid-row: 1 / span 2;
+      grid-column: 2 / span 3  ;
+      grid-row: 1/ span 1 ;
+    }
+
+    .tt3{
+      grid-column: 1 / span 1 ;
+      grid-row: 2 ;
+
+    }
+
+    .tt4{
+      grid-column: 2 / span 1;
+      grid-row: 2 ;
+
+    }
+
+    .tt5{
+      grid-column: 3 /span 1 ;
+      grid-row: 2 ;
+
     }
 
 `
+
+const Line1 = styled.div`
+border-radius: 20px;
+padding:20px;
+border:1px solid #ebe9f1;
+background-color: white;
+margin: 20px 20px 0 0 ;
+
+`
+const Line2 = styled.div`
+border-radius: 20px;
+padding:20px;
+border:1px solid #ebe9f1;
+background-color: white;
+`
+const Line3 = styled.div`
+border-radius: 20px;
+padding:20px;
+border:1px solid #ebe9f1;
+background-color: white;`
+
+const Line4 = styled.div`
+border-radius: 20px;
+padding:20px;
+border:1px solid #ebe9f1;
+background-color: white;`
